b142cbebe4918f824a50b801b2ff7ac7c7275cf6
Chris Diehl <jjchrisdiehl@gmail.com>
2023-08-25 20:11:21 -0700

    Pass selectedSiteData to CheckoutSummaryFeaturesListDomainItem

--- client/my-sites/checkout/composite-checkout/components/wp-checkout.tsx
+++ client/my-sites/checkout/composite-checkout/components/wp-checkout.tsx
@@ -413,6 +413,7 @@ export default function WPCheckout( {
 							<CheckoutSummaryBody className="checkout__summary-body">
 								<WPCheckoutOrderSummary
 									siteId={ siteId }
+									selectedSiteData={ selectedSiteData }
 									onChangeSelection={ changeSelection }
 									nextDomainIsFree={ responseCart?.next_domain_is_free }
 								/>
